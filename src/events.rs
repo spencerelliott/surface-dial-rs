c23c6a27ad910d6253e5a0603307b449cb9ea557
Spencer Elliott <s.elliott123@gmail.com>
2022-06-21 22:24:29 -0500

    Working version of library

--- src/events.rs
+++ src/events.rs
@@ -1,23 +1,28 @@
+#[derive(Debug)]
 pub enum DialDirection {
     Clockwise,
     Counterclockwise
 }
 
+#[derive(Debug)]
 pub enum DialEvent {
-    Rotate { direction: DialDirection, velocity: u32 },
+    Rotate { direction: DialDirection, velocity: i16 },
     Button { pressed: bool }
 }
 
+#[derive(Debug)]
 pub enum ConnectionEvent {
     Connect,
     Disconnect
 }
 
+#[derive(Debug)]
 pub enum TopLevelEvent {
     DialEvent(DialEvent),
     ConnectionEvent(ConnectionEvent)
 }
 
+#[derive(Debug)]
 pub enum ThreadSignals {
     SendHaptics([u8; 8]),
     End
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
 extern crate hidapi;
-mod events;
 
-use std::thread::{self, JoinHandle};
+pub mod events;
+
+use std::{thread::{self, JoinHandle}, borrow::Borrow, time};
 
 use flume::{Sender, Receiver};
-use hidapi::HidDevice;
+use hidapi::{HidDevice, HidApi};
 
 use events::{DialEvent, ConnectionEvent, TopLevelEvent, ThreadSignals};
+
+use crate::events::DialDirection;
+
+const MSFT_VENDOR_ID: u16 = 0x045e;
+const SURFACE_DIAL_DEVICE_ID: u16 = 0x091b;
 
 pub struct SurfaceDial<'a> where  {
     subdivisions: u16,
@@ -18,6 +24,10 @@ pub struct SurfaceDial<'a> where  {
     signal_tx: Sender<ThreadSignals>,
 }
 
+struct DialState {
+    button_state: bool
+}
+
 impl<'a> SurfaceDial<'a> {
     /// Creates a new `SurfaceDial` structure and begins the process of searching and
     /// connecting to a dial.
@@ -27,30 +37,119 @@ impl<'a> SurfaceDial<'a> {
         let (signal_tx, signal_rx) = flume::unbounded::<ThreadSignals>();
 
         let handler = thread::spawn(move || {
-            let mut still_running = false;
-            // Create the context for the HID device
+            let mut still_running = true;
+            let mut opened_device: Option<HidDevice> = None;
+
+            let mut read_buffer: [u8; 10] = [0; 10];
+
+            let mut dial_state = DialState {
+                button_state: false
+            };
+
+            let mut current_haptic_bytes: [u8; 8] = [0x1, (0x0 & 0xff) as u8, ((0x0 >> 8) & 0xff) as u8, 0x0, 0x3, 0x0, 0x0, 0x0];
 
             while still_running {
                 // Check for thread messages
-                for e in signal_rx.iter() {
+                for e in signal_rx.try_iter() {
                     match e {
                         ThreadSignals::End => still_running = false,
                         ThreadSignals::SendHaptics(h) => {
-
+                            current_haptic_bytes = h;
+                            
+                            if let Some(d) = opened_device.borrow() {
+                                // Write haptics
+                                d.send_feature_report(&h).expect("Could not set up haptics");
+                            }
                         }
                     }
                 }
 
                 // Try to connect if not connected
+                match opened_device.borrow() {
+                    // Create the context for the HID device
+                    None => {
+                        match HidApi::new() {
+                            Ok(api) => {
+                                for device in api.device_list() {
+                                    if device.vendor_id() == MSFT_VENDOR_ID && device.product_id() == SURFACE_DIAL_DEVICE_ID {
+                                        println!("Found Surface Dial!");
+                                        let result = device.open_device(&api);
 
-                // Process any messages
+                                        match result {
+                                            Ok(d) => {
+                                                // Write haptics
+                                                d.send_feature_report(&current_haptic_bytes).expect("Could not set up haptics");
 
-                // Loop while getting new events
-                
-                // d.send_feature_report(&haptic_bytes).expect("Could not set up haptics on Surface Dial");
-                
-                // Send new events
-                event_tx.send(TopLevelEvent::ConnectionEvent(ConnectionEvent::Connect)).expect("Could not send message");
+                                                opened_device = Some(d);
+
+                                                event_tx.send(TopLevelEvent::ConnectionEvent(ConnectionEvent::Connect)).expect("Could not send message");
+
+                                                break;
+                                            },
+                                            Err(_e) => {
+                                                event_tx.send(TopLevelEvent::ConnectionEvent(ConnectionEvent::Disconnect)).expect("Could not send message");
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if let None = opened_device.borrow() {
+                                    thread::sleep(time::Duration::from_millis(250));
+                                }
+                            },
+                            Err(e) => {
+                                eprintln!("Error: {}", e);
+                            }
+                        }
+                    },
+                    // Process any messages
+                    Some(d) => {
+                        let result = d.read_timeout(&mut read_buffer, 10);
+
+                        match result {
+                            Ok(size) => {
+                                if size > 0 && read_buffer[0] == 0x1 {
+                                    // Check whether the button was pressed (either 0x1 or 0x3 is returned in the button byte)
+                                    let button_pressed = if read_buffer[1] == 0x1 || read_buffer[1] == 0x3 {
+                                        true
+                                    } else {
+                                        false
+                                    };
+
+                                    // Send a new event depending on the previous state of the button
+                                    if button_pressed != dial_state.button_state {
+                                        dial_state.button_state = button_pressed;
+                                        event_tx.send(TopLevelEvent::DialEvent(DialEvent::Button { pressed: button_pressed })).expect("Could not send message");
+                                    }
+
+                                    // Make sure there was enough read from the button and the velocity actually changes
+                                    if size > 2 && read_buffer[2] > 0 {
+                                        // The velocity goes from -127 to 127 so we have to convert the byte into a signed value
+                                        let adjusted_velocity: i16 = if read_buffer[2] > 128 {
+                                            read_buffer[2] as i16 - 256i16
+                                        } else {
+                                            read_buffer[2] as i16
+                                        };
+
+                                        // Determine the direction of the dial based on the velocity
+                                        let direction: DialDirection = if read_buffer[2] <= 128 {
+                                            DialDirection::Clockwise
+                                        } else {
+                                            DialDirection::Counterclockwise
+                                        };
+
+                                        // Send the velocity event
+                                        event_tx.send(TopLevelEvent::DialEvent(DialEvent::Rotate { direction: direction, velocity: adjusted_velocity })).expect("Could not send message");
+                                    }
+                                }
+                            },
+                            Err(_e) => {
+                                opened_device = None;
+                                event_tx.send(TopLevelEvent::ConnectionEvent(ConnectionEvent::Disconnect)).expect("Could not send message");
+                            }
+                        } 
+                    }
+                }
             }
         });
 
@@ -109,7 +208,7 @@ impl<'a> SurfaceDial<'a> {
     /// Processes all of the events currently queued in the buffer. This should be called
     /// fairly often to make sure the buffer does not fill up.
     pub fn process(&self) {
-        for e in self.event_rx.iter() {
+        for e in self.event_rx.try_iter() {
             match e {
                 TopLevelEvent::ConnectionEvent(c) => (self.on_connection_event)(c),
                 TopLevelEvent::DialEvent(d) => (self.on_event)(d),
